4ab939cea5b088c7777f99326f6605f2812d9ef8
gesqueda <gerardo.esqueda9479@alumnos.udg.mx>
2026-03-13 21:59:33 -0600

    chore(estructura) -> correcion de la estructura de directorios, elimine plantilla de react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p class="text-black">Hola mundo</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
