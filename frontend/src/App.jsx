f4b9fdc531ac3c4a19f790023c7f95138c078884
CheyPrime08 <christopher.santana8173@alumnos.udg.mx>
2026-04-04 12:40:57 -0600

    feat -> Revisando Front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,11 @@ import Agregar from "./pages/admin/Agregar";
 import Modificar from "./pages/admin/Modificar";
 import Eliminar from "./pages/admin/Eliminar";
 
-function Home() {
-  return (
-    <div>
-      <h1>Menú Principal</h1>
-      <nav>
-        <Link to="/agregar">Ir a Agregar</Link>
-        <Link to="/modificar">Ir a Modificar</Link>
-        <Link to="/eliminar">Ir a Eliminar</Link>
-      </nav>
-    </div>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/Agregar" />} />
         <Route path="/agregar" element={<Agregar />} />
         <Route path="/modificar" element={<Modificar />} />
         <Route path="/eliminar" element={<Eliminar />} />
