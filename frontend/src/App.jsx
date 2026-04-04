5f67476153c9f11a00b484b49e124de539ba4c0a
CheyPrime08 <christopher.santana8173@alumnos.udg.mx>
2026-04-04 12:38:50 -0600

    feat -> Revisando Front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,18 @@ import Agregar from "./pages/admin/Agregar";
 import Modificar from "./pages/admin/Modificar";
 import Eliminar from "./pages/admin/Eliminar";
 
+function Home() {
+  return (
+    <div>
+      <h1>Menú Principal</h1>
+      <nav>
+        <Link to="/agregar">Ir a Agregar</Link>
+        <Link to="/modificar">Ir a Modificar</Link>
+        <Link to="/eliminar">Ir a Eliminar</Link>
+      </nav>
+    </div>
+  );
+}
 
 function App() {
   return (
