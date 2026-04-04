5831750863f92b98e9094727263c6e7b7a631520
CheyPrime08 <christopher.santana8173@alumnos.udg.mx>
2026-04-04 11:48:41 -0600

    (feat) -> Incorporación del front al LocalHost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Agregar from "./pages/admin/agregar.jsx";
-import Modificar from "./pages/admin/modificar.jsx";
-import Eliminar from "./pages/admin/eliminar.jsx";
-import "./index.css";
-
-function Home() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Menú Principal</h1>
-      <nav className="flex flex-col gap-4">
-        <Link to="/agregar" className="menu-opcion">Ir a Agregar</Link>
-        <Link to="/modificar" className="menu-opcion">Ir a Modificar</Link>
-        <Link to="/eliminar" className="menu-opcion">Ir a Eliminar</Link>
-      </nav>
-    </div>
-  );
-}
+import Agregar from "./pages/admin/agregar";
+import Modificar from "./pages/admin/modificar";
+import Eliminar from "./pages/admin/eliminar";
 
 function App() {
   return (
