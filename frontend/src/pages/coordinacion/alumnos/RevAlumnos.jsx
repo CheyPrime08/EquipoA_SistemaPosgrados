ac469eb9c9fa5560488e33f8dddfa2f058704e59
diego-ornelas4952 <diego.ornelas2005>
2026-04-13 11:23:16 -0600

    (fix) -> Cambio de directorio

--- frontend/src/pages/coordinacion/alumnos/RevAlumnos.jsx
+++ frontend/src/pages/coordinacion/alumnos/RevAlumnos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { CoordSidebar } from "@/components/coordinacion/shared/CoordSidebar";
 import { CoordHeader } from "@/components/coordinacion/shared/CoordHeader";
-import RevisionAlumnado from "@/modules/coordinacion/alumnos/RevisionAlumnado";
+import RevisionAlumnado from "@/modules/coordinador/alumnos/RevisionAlumnado";
 
 export default function RevAlumnos() {
     return (
