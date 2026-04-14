14eb4fa40145caa8a32409ab8be42cf3abe720c6
Carlos-gh00 <gutierrezhernandezj299@gmail,com>
2026-04-13 19:00:14 -0600

    Revert "(chore)-> reestructuracion de el programa a la estructura de atomic desing"
    This reverts commit 2d1cb2681912c2ac3ea63bf70131ec650581dd1e.

--- frontend/src/pages/coordinacion/agenda/HomeCoord.jsx
+++ frontend/src/pages/coordinacion/agenda/HomeCoord.jsx
@@ -1,13 +1,22 @@
-import { CoordLayout } from "@/components/templates/CoordLayout";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { CoordSidebar } from "@/components/coordinacion/shared/CoordSidebar";
+import { CoordHeader } from "@/components/coordinacion/shared/CoordHeader";
 import { CoordPanel } from "@/components/coordinacion/shared/CoordPanel";
 
 const HomeCoord = () => {
   return (
-    <CoordLayout>
-      <CoordPanel>
-        <p>hola mundo</p>
-      </CoordPanel>
-    </CoordLayout>
+    <SidebarProvider className="flex-col h-screen overflow-hidden">
+      <CoordHeader />
+
+      <div className="flex flex-1 overflow-hidden relative">
+        <CoordSidebar />
+        <SidebarInset className="bg-sidebar flex flex-1 overflow-hidden">
+          <CoordPanel>
+            <p>hola mundo</p>
+          </CoordPanel>
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
   );
 };
 
