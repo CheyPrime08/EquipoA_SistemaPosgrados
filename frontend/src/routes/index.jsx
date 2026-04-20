c9dcd93a54b77c8607e91431d68b72b4a4502142
AngelSH <angel.serrano9061@alumnos.udg.mx>
2026-04-19 20:06:52 -0600

    [Fix] -> arreglos  de rutas coordinador y login

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RediPreregistro from "../pages/aspirante/rediPreregistro";
-import Login from "../pages/login/login";
-import RecuPasswotd from "../pages/login/password/recuPasswotd";
+import { Route, Routes } from "react-router-dom";
+import Login from "@/pages/login/login";
+import RecuPasswotd from "@/pages/login/password/recuPasswotd";
+import RediPreregistro from "@/pages/aspirante/rediPreregistro";
 import InicioCoord from "@/pages/coordinador/inicio/InicioCoord";
-import ArchivoCoord from "@/pages/coordinador/archivo/ArchivoCoord";
-import ConvocatoriaCoord from "@/pages/coordinador/aspirante/ConvocatoriaCoord";
+import ArchivoCoord from "@/pages/coordinador/inicio/ArchivoCoord";
+import ConvocatoriaCoord from "@/pages/coordinador/convocatoria/ConvocatoriaCoord";
 import CicloCoord from "@/pages/coordinador/ciclo/CicloCoord";
-import AgendaCoord from "@/pages/coordinador/agenda/AgendaCoord";
+import AgendaCoord from "@/pages/coordinador/inicio/AgendaCoord";
 import PendientesCoord from "@/pages/coordinador/inicio/PendientesCoord";
 import AjustesCoord from "@/pages/coordinador/inicio/AjustesCoord";
 
-const routesAspirante = [
+const routesLogin = [
   {
     path: "/rediPreregistro",
     element: <RediPreregistro />,
@@ -59,7 +59,7 @@ const routesCoordinador = [
 export default function Rutas() {
   return (
     <Routes>
-      {routesAspirante.map((route, index) => (
+      {routesLogin.map((route, index) => (
         <Route key={index} path={route.path} element={route.element} />
       ))}
       {routesCoordinador.map((route, index) => (
